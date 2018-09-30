67640a03760b175646771481cab03af474793ebf
bjelinek26 <brad.jelinek.226@gmail.com>
2018-09-30 16:39:03 -0400

    On branch parse_tty Changes to be committed:
    added detection of new $(tty) of the new terminal that is opened,
    path of new tty does not have to be hardcoded

--- up.sh
+++ up.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+TTY=$(tty)
+
+IFS="/"
+
+read -r NOTHIN DEV PTS NUM <<< TTY
+
+NEW_PTS=$(($NUM + 1))
+
 sudo apt update
 
-terminology &
+gnome-terminal --window &
 
-apt list --upgradable >> /dev/pts/1
+apt list --upgradable >> /dev/pts/"${NEW_PTS}"
  
 sudo apt update -y
